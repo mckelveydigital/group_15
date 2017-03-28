46a69b20df11c1d6db2b7f5a865501eaddbf2ebf
Luke McKelvey <mckelvey-l2@email.ulster.ac.uk>
2017-03-28 15:45:55 +0100

    --Luke Mckelvey - Added gulp to the project to allow for custom javascript files to be used along side angular2. The javascript is concatenated into one file and minified to save load times.

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    connect = require('gulp-connect'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+    
+var sassSources = ['sass/*.scss'];
+
+var jsSources = ['js/custom/*.js'];
+
+
+/*
+	
+var sassSources = ['sass/*.scss'];
+
+gulp.task('sass', function() {
+  gulp.src(sassSources)
+  .pipe(sass({style: 'expanded'}))
+    .on('error', gutil.log)
+  .pipe(concat('style.css'))
+  .pipe(gulp.dest('css/'))
+  .pipe(connect.reload())
+});
+*/
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+  .pipe(uglify())
+  .pipe(concat('all.js'))
+  .pipe(gulp.dest('src/assets/js/'))
+  .pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+  gulp.watch(jsSources, ['js']);
+  gulp.watch(sassSources, ['sass']);
+});
+
+gulp.task('default', ['watch']);
