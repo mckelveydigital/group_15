7d0ed37fc31bc9b4b6716a297015737f30e4a1d0
Luke McKelvey <mckelvey-l2@email.ulster.ac.uk>
2017-04-05 21:36:35 +0100

    --Luke McKelvey - Developed admin form section further

--- js/custom/luke.js
+++ js/custom/luke.js
@@ -1,3 +1,11 @@
 $(document).on('click', '.match', function(){
 	$(this).toggleClass("on");
 });
+
+$(document).on('change', '#team_select', function(){
+
+	var theteam = $("#team_select").val();
+
+	$("#edit_team_btn").attr("href","/admin/edit/" + theteam);
+	
+});
