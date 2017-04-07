d3625a3bf9cf5fac4c3bbb91f84c5d191fd3b1db
Luke McKelvey <mckelvey-l2@email.ulster.ac.uk>
2017-04-07 01:01:39 +0100

    --Luke Mckelvey - Fixed some css issues

--- js/custom/luke.js
+++ js/custom/luke.js
@@ -9,3 +9,34 @@ $(document).on('change', '#team_select', function(){
 	$("#edit_team_btn").attr("href","/admin/edit/" + theteam);
 	
 });
+
+window.fbAsyncInit = function() {
+FB.init({
+  appId      : '1282394385202069',
+  xfbml      : true,
+  version    : 'v2.8'
+});
+FB.AppEvents.logPageView();
+  FB.api(
+  '/uefachampionsleague/feed?access_token=EAACEdEose0cBAP3gkPOcFhPVFBbRa7AnZAZCg9R6ZAZCgYiM1iv0EU14dfyOzoUHQoIgYFrzZCTguATN0U85IEBJVOgeYk3nmMwzIg9MeHhUzWD0hxZAbMgZC3kSfcZBpYCEJuZALAL1S7DevFrYj2NK8TksxUS4wspjv2uwOm3ss1baDa8S2MWORxnDeNtESiFAZD',
+  'GET',
+  {"limit":"3"},
+  function(response) {
+      // Insert your code here
+      console.log(response.data[1]);
+     
+     $(document).find("#fb_message_0").html(response.data[0].message);
+     $(document).find("#fb_message_1").html(response.data[1].message);
+     $(document).find("#fb_message_2").html(response.data[2].message);
+  }
+);
+
+};
+
+(function(d, s, id){
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) {return;}
+ js = d.createElement(s); js.id = id;
+ js.src = "//connect.facebook.net/en_US/sdk.js";
+ fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
