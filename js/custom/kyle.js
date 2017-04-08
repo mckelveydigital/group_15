3bed751acbee926a784445fb01fe2fe8a300bc60
Kyle Ferguson <kyleferguson@Kyles-MacBook-Pro-4.local>
2017-04-08 12:54:24 +0100

    --Added loading feature, added Youtube video with, added youtube social link, revisted colour scheme

--- js/custom/kyle.js
+++ js/custom/kyle.js
@@ -1,10 +1,30 @@
-if( jQuery(".toggle .toggle-title").hasClass('active') ){
-		jQuery(".toggle .toggle-title.active").closest('.toggle').find('.toggle-inner').show();
-	}
-	jQuery(".toggle .toggle-title").click(function(){
-		if( jQuery(this).hasClass('active') ){
-			jQuery(this).removeClass("active").closest('.toggle').find('.toggle-inner').slideUp(200);
-		}
-		else{	jQuery(this).addClass("active").closest('.toggle').find('.toggle-inner').slideDown(200);
-		}
-	});
+// Loads the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Replaces the 'ytplayer' element with an <iframe> and
+  // YouTube player after the API code downloads.
+  var player;
+  function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+        height: '80%',
+        width: '100%',
+        videoId: 'vaYIJ34rKYk',
+        playerVars: {
+          autoplay: 1,
+          controls: 0,
+          loop: 1,
+          showinfo: 0,
+          rel: 0
+        },
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+  }
+
+function onPlayerReady(event){
+    player.mute();
+}
